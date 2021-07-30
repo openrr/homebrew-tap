ba75bcd9bf73c3ffb77ddee4c8d859fda7550d96
Taiki Endo <taiki@smilerobotics.com>
2021-07-30 13:38:38 +0900

    Update urdf-viz to 0.30.0

--- Formula/urdf-viz.rb
+++ Formula/urdf-viz.rb
@@ -3,17 +3,17 @@
 class UrdfViz < Formula
   desc "URDF visualizer"
   homepage "https://github.com/openrr/urdf-viz"
-  version "0.29.0"
+  version "0.30.0"
   license "Apache-2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/openrr/urdf-viz/releases/download/v0.29.0/urdf-viz-x86_64-apple-darwin.tar.gz"
-    sha256 "f8b359f1bfda757912656d7b682aaddac047c56acf2f7287cb3146689ee0a596"
+    url "https://github.com/openrr/urdf-viz/releases/download/v0.30.0/urdf-viz-x86_64-apple-darwin.tar.gz"
+    sha256 "4d510ff39db6576ef8e05db8834371e29f0a58844d8c8ae629901586d2170ee7"
   end
   if OS.linux?
-    url "https://github.com/openrr/urdf-viz/releases/download/v0.29.0/urdf-viz-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "c469a5c27b2c6a0712ef371e827107ef1e90fc836f8e010269ff156960bdcb47"
+    url "https://github.com/openrr/urdf-viz/releases/download/v0.30.0/urdf-viz-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "f6ffca32843e41a3a80cf75d02ea6950416d309e6aa08b9733307394d1115317"
   end
 
   def install
