bf74a65b23367bd74b24b3d3b6acd6328eb03017
Taiki Endo <taiki@smilerobotics.com>
2023-05-02 01:34:55 +0000

    Update urdf-viz to 0.43.0

--- Formula/urdf-viz.rb
+++ Formula/urdf-viz.rb
@@ -3,19 +3,19 @@
 class UrdfViz < Formula
   desc "URDF visualizer"
   homepage "https://github.com/openrr/urdf-viz"
-  version "0.42.0"
+  version "0.43.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/openrr/urdf-viz/releases/download/v0.42.0/urdf-viz-x86_64-apple-darwin.tar.gz"
-      sha256 "0e4dd3462bc7c534520dccf9c7924b2f990c24e9876e11e81e6d088dea8ecbea"
+      url "https://github.com/openrr/urdf-viz/releases/download/v0.43.0/urdf-viz-x86_64-apple-darwin.tar.gz"
+      sha256 "c78502937c4cd399d2da2842d78858b2b77effcf011f126dd0f54735228490fc"
     end
   end
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/openrr/urdf-viz/releases/download/v0.42.0/urdf-viz-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "6c028ce70a8ac6ea1277b08366b220fac35c331b01a49cabba82d34d443fdfa0"
+      url "https://github.com/openrr/urdf-viz/releases/download/v0.43.0/urdf-viz-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "118365557e26e168cb8a64b4cfaf66685b07bacb4ae5948c0b1d87ca7ba1d75d"
     end
   end
 
