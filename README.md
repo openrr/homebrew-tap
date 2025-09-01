5e3efd01483423826a600d5c8d34af3f1b622eaa
Taiki Endo <te316e89@gmail.com>
2025-09-01 13:01:41 +0900

    Update formula update code

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Homebrew tap for projects under github.com/openrr.
 brew install openrr/tap/<package>
 ```
 
-The mechanism of this repository is based on [taiki-e/homebrew-tap](https://github.com/taiki-e/homebrew-tap).
+The mechanism of this repository is based on [taiki-e/homebrew-tap](https://github.com/taiki-e/homebrew-tap/tree/9cfc23c03bcfc37b41f01c9f73e90706bb55bb1b).
 
 ## Supported packages
 
---
+++ tools/ci/formula.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -CeEuo pipefail
+IFS=$'\n\t'
+trap -- 's=$?; printf >&2 "%s\n" "${0##*/}:${LINENO}: \`${BASH_COMMAND}\` exit with ${s}"; exit ${s}' ERR
+cd -- "$(dirname -- "$0")"/../..
+
+bail() {
+  printf >&2 'error: %s\n' "$*"
+  exit 1
+}
+
+if [[ -z "${CI:-}" ]]; then
+  bail "this script is intended to call from release workflow on CI"
+fi
+
+git config user.name 'Taiki Endo'
+git config user.email "taiki@smilerobotics.com"
+
+set -x
+
+has_update=''
+for formula in Formula/*.rb; do
+  git add -N "${formula}"
+  if ! git diff --exit-code -- "${formula}" &>/dev/null; then
+    name="${formula##*/}"
+    name="${name%.*}"
+    version=$(grep -E '^\s+version "' "${formula}" | sed -E 's/^\s+version "//; s/"$//')
+    git add "${formula}"
+    git commit -m "Update ${name} to ${version}"
+    has_update=1
+  fi
+done
+
+if [[ -n "${has_update}" ]] && [[ -n "${GITHUB_OUTPUT:-}" ]]; then
+  printf 'success=false\n' >>"${GITHUB_OUTPUT}"
+fi
